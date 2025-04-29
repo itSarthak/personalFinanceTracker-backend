ac826fb431dc525f88c08a52dd0dac2cd950dbfa
Sarthak . <sarthakt3006@gmail.com>
2025-04-29 21:44:15 +0530

    Add: Allowed CORS policy for Income APIs

--- src/main/java/com/sarthak/expenseTracker/controller/IncomeController.java
+++ src/main/java/com/sarthak/expenseTracker/controller/IncomeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1")
 @Slf4j
